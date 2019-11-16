1947f9f7e93740c7a36ca867aea43f1af3a45f4c
ma <554121157@qq.com>
2019-11-16 15:49:34 +0800

    第1次提交

---
+++ src/main/java/A.java
@@ -0,0 +1,13 @@
+/**
+ * @author Mafuyou
+ * @version 1.0
+ * @Description
+ * @Date 2019/11/16 15:48
+ * @since
+ * @deprecated
+ */
+public class A {
+    public static void main(String[] args) {
+        System.out.println("11111111");
+    }
+}
