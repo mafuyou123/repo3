02cb7f7ba36ba299f990fe20379a57cecb8cf10b
ma <554121157@qq.com>
2019-11-16 16:16:32 +0800

    A第6次提交

--- src/main/java/A.java
+++ src/main/java/A.java
@@ -13,5 +13,6 @@ public class A {
         System.out.println("33333333");
         System.out.println("55555555");
         System.out.println("66666666");
+        System.out.println("77777777");
     }
 }
