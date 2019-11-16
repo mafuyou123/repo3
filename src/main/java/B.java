6410bd5c45d1b5f445987c5fd6be39cc42e569f1
ma <554121157@qq.com>
2019-11-16 16:00:35 +0800

    B第1次提交

---
+++ src/main/java/B.java
@@ -0,0 +1,13 @@
+/**
+ * @author Mafuyou
+ * @version 1.0
+ * @Description
+ * @Date 2019/11/16 15:58
+ * @since
+ * @deprecated
+ */
+public class B {
+    public static void main(String[] args) {
+        System.out.println("aaa");
+    }
+}
